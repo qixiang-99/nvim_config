febc1a2347e4df880e81f5e8412724b9efb29372
qixiang-99 <203170375+qixiang-99@users.noreply.github.com>
2025-09-29 19:20:25 -0700

    feat: add keymaps for yanking to clipboard in visual and normal modes
    Signed-off-by: qixiang-99 <203170375+qixiang-99@users.noreply.github.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("v", "<leader>y", require("osc52").copy_visual, { desc = "Yank to clipboard" })
+vim.keymap.set("n", "<leader>C", require("osc52").copy_operator, { expr = true, desc = "Yank to clipboard" })
