ed2bb99c525cd495a048653c227e9046dd6f5216
root <root@prenyx0051.a51.clusters.nvidia.com>
2025-11-04 10:27:52 -0800

    use lazyvim extra to config clangd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,49 +9,7 @@ return -- add pyright to lspconfig
       ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = { mason = true},
-        clangd = {
-          mason = true,
-          keys = {
-            { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-          },
-          root_dir = function(fname)
-            -- Ensure fname is a string (could be a buffer number)
-            if type(fname) ~= "string" then
-              fname = vim.api.nvim_buf_get_name(fname)
-            end
-            return require("lspconfig.util").root_pattern(
-              "Makefile",
-              "configure.ac",
-              "configure.in",
-              "config.h.in",
-              "meson.build",
-              "meson_options.txt",
-              "build.ninja"
-            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-              fname
-            ) or require("lspconfig.util").find_git_ancestor(fname)
-          end,
-          capabilities = {
-            offsetEncoding = { "utf-16" },
-          },
-          cmd = {
-            -- "clangd-12",
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--header-insertion=iwyu",
-            "--completion-style=detailed",
-            "--function-arg-placeholders",
-            "--fallback-style=llvm",
-            "--query-driver=/usr/bin/g++",
-          },
-          init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-          },
-        },
+        pyright = { mason = true },
       },
     },
   },
